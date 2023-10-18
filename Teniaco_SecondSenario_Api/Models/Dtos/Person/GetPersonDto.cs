8ff02371bbb36b0e3591ff78d33cee879b4a76c0
Ari23yan <ariyan137923@yahoo.com>
2023-10-18 16:50:14 +0330

    20231018-FirstPush

--- Teniaco_SecondSenario_Api/Models/Dtos/Person/GetPersonDto.cs
+++ Teniaco_SecondSenario_Api/Models/Dtos/Person/GetPersonDto.cs
@@ -5,3 +5,4 @@
         public Int64  Id { get; set; }
     }
 }
+    
---
+++ Teniaco_SecondSenario_Api/Models/Dtos/Person/UpdatePersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Teniaco_SecondSenario_Api.Models.Dtos.Person
+{
+    public class UpdatePersonDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string? Mobile { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
